07d99fa7eb911d44cc346da54dbca1b9567a7d11
polemikal <68484486+polemikal@users.noreply.github.com>
2021-08-31 22:49:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const { Intents } = require('discord.js');
 const { TOKEN, CLIENT_ID, GUILD_ID } = require("./config.json");
 const AClient = require('./utils/structures/AClient.js');
 
 (async() => {
-    const client = new AClient(CLIENT_ID, TOKEN);
+    const client = new AClient(CLIENT_ID, TOKEN, Object.keys(Intents.FLAGS));
     await client.loadEvents('../../events');
     await client.loadCommands('../../commands', GUILD_ID)
     await client.connect();
